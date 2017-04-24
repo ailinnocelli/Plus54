5c357526c8d8e3d89adc344b270ef8e0d6572d2d
ailinnocelli <ailin.nocelli@gmail.com>
2017-04-23 21:23:31 -0300

    modifico modal y margenes

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,29 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
 	$('.carousel').carousel();
-})*/
+
+
+$(document).ready(function(){
+    $(".img-modal").click(function(e){
+    	var imgPath= $(e.toElement).attr("data-src-full");
+    	$("#imgModal").attr("src",imgPath);
+        $("#myModal").modal('show');
+    });
+});
+
+
+	/*$.ajax({
+		url: "json/info.json",
+		error: function(e) {
+			$('#info').html('<p>An error has occurred</p>');
+			alert('hola error');
+		},
+		success: function(data) {
+			var $title = $('<h1>').text(data.talks[0].talk_title);
+			var $description = $('<p>').text(data.talks[0].talk_description);
+			$('#info')
+			.append($title)
+			.append($description);
+		},
+		type: 'GET'
+	});*/
+})
